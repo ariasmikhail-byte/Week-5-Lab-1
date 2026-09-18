185ba63dc63b92b25aad48b8106fb2be84de1e47
arias.mikhail <arias.mikhail@student.ccm.edu>
2026-09-18 10:51:53 -0400

    Still working on it.

---
+++ temperature_category.py
@@ -0,0 +1,15 @@
+# Mikhail Arias 
+# CMP - 131
+# Week 5
+# Lab 1
+print("===============================")
+print("     Temperature Catagorizer:")
+
+temp = float(input("Enter the temperature in degrees Fahrenheit"))
+
+if temp < 80:
+    print("Its Hot")
+    catagory = "Cold"
+    print("Its Hot outside")
+    
+    
